af498214ee4c7945aa4be2c1d252dcfc7ca9cd30
Mohammad Alazmeh <mohamadalazmeh4@gmail.com>
2025-07-17 21:48:21 +0400

    refactor home screen

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:habit_tracker/core/dependency_injection/injection.dart';
 import 'package:habit_tracker/core/theme/theme_cubit.dart';
+import 'package:habit_tracker/features/calendar/presentation/screens/calendar_screen.dart';
+import 'package:habit_tracker/features/habit/presentation/controller/habit_bloc.dart';
 import 'package:habit_tracker/features/habit/presentation/screens/habits_screen.dart';
 
 class HomeScaffold extends StatefulWidget {
@@ -15,58 +18,60 @@ class _HomeScaffoldState extends State<HomeScaffold> {
 
   final List<Widget> _pages = const [
     HabitsScreen(),
-    Placeholder(), // CalendarPage() later
+    CalendarScreen(),
     Placeholder(), // StatisticsPage() later
   ];
 
-  final List<String> _pageTitles = const [
-    'Habits',
-    'Calendar',
-    'Statistics',
-  ];
-
+  final List<String> _pageTitles = const ['Habits', 'Calendar', 'Statistics'];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_pageTitles[_currentIndex]),
-        actions: [
-          IconButton(
-            icon: AnimatedSwitcher(
-              duration: Durations.long1,
-              transitionBuilder: (child, animation) {
-                return ScaleTransition(
-                  scale: animation,
-                  child: child,
-                );
-              },
-              child: context.watch<ThemeCubit>().state == ThemeMode.dark
-                  ? const Icon(Icons.light_mode, key: ValueKey('light_mode_icon'))
-                  : const Icon(Icons.dark_mode, key: ValueKey('dark_mode_icon')),
+    return BlocProvider(
+      create: (context) =>
+          getIt<HabitBloc>()..add(const HabitEvent.loadHabits()),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(_pageTitles[_currentIndex]),
+          actions: [
+            IconButton(
+              icon: AnimatedSwitcher(
+                duration: Durations.long1,
+                transitionBuilder: (child, animation) {
+                  return ScaleTransition(scale: animation, child: child);
+                },
+                child: context.watch<ThemeCubit>().state == ThemeMode.dark
+                    ? const Icon(
+                        Icons.light_mode,
+                        key: ValueKey('light_mode_icon'),
+                      )
+                    : const Icon(
+                        Icons.dark_mode,
+                        key: ValueKey('dark_mode_icon'),
+                      ),
+              ),
+              onPressed: () => context.read<ThemeCubit>().toggleTheme(),
             ),
-            onPressed: () => context.read<ThemeCubit>().toggleTheme(),
-          ),
-        ],
-      ),
-      body: IndexedStack(index: _currentIndex, children: _pages),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.checklist_rtl_rounded),
-            label: 'Today',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            label: 'Calendar',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bar_chart),
-            label: 'Statistics',
-          ),
-        ],
+          ],
+        ),
+        body: IndexedStack(index: _currentIndex, children: _pages),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) => setState(() => _currentIndex = index),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.checklist_rtl_rounded),
+              label: 'Today',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: 'Calendar',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bar_chart),
+              label: 'Statistics',
+            ),
+          ],
+        ),
       ),
     );
   }
